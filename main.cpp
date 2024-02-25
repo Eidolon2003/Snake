87c09b8b8a66051a579acfdaa072f421d007aa07
Julian Williams <julian@wazworld.net>
2024-02-25 00:20:40 -0600

    Game finished

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "snake.h"
 
 int main() {
+	constexpr uint16_t SCREEN_SIZE = (Snake::NUM_TILES + 2) * Snake::TILE_SIZE + Snake::GAP_SIZE;
+
 	jaw::EngineProperties ep;
 	jaw::AppProperties ap;
+	ap.layerCount = 2;
+	ap.backgroundCount = 1;
+	ap.framerate = 10;
+	ap.size = jaw::Point(SCREEN_SIZE, SCREEN_SIZE);
+	ap.title = "Snake";
+
 	jaw::StartEngine(new Snake, ap, ep);
 }
--- snake.h
+++ snake.h
@@ -1,7 +1,27 @@
 #include <JawEngine.h>
+#include <queue>
 
 class Snake : public jaw::AppInterface {
+	static const uint32_t BORDER_COLOR = 0x1111EE;
+	static const uint32_t SNAKE_COLOR = 0x11EE11;
+	static const uint32_t APPLE_COLOR = 0xEE1111;
+
+	std::list<jaw::Point> snakeParts;
+	jaw::Point applePos;
+
+	enum DIR { UP, DOWN, LEFT, RIGHT, NONE };
+	std::queue<DIR> inputQueue;
+	DIR snakeDir = NONE;
+
+	void Reset();
+	jaw::Point RandPos() const;
+	void DrawTile(uint32_t color, jaw::Point tile) const;
+
 public:
+	static const uint8_t NUM_TILES = 32;
+	static const uint8_t TILE_SIZE = 16;
+	static const uint8_t GAP_SIZE = 2;
+
 	void Init() override;
 	void Loop() override;
 };
